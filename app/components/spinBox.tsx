ebc903f5c78ccfd4a03b5d1d15c78460acfddbcb
Davitmach <wvime30@gmail.com>
2025-07-04 15:03:16 +0400

    1

--- app/components/spinBox.tsx
+++ app/components/spinBox.tsx
@@ -2,6 +2,6 @@ import Image from "next/image"
 
 export const SpinBox = (props:{img:string})=> {
     return(
-        <div className="shrink-0 w-[200px] h-[256px] rounded-[20px] overflow-hidden"><Image quality={100} alt="w" width={200} height={256} src={props.img}/></div>
+        <div className="spinbox-side shrink-0 w-[200px] h-[256px] rounded-[20px] overflow-hidden"><Image quality={100} alt="w" width={200} height={256} src={props.img}/></div>
     )
 }
